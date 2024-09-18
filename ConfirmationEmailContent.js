ad7371df698a5f811c6758fd0d438dac4f19a5b5
Prashant Patel <itspraskpatel@gmail.com>
2024-09-19 01:30:33 +0530

    modifcation to fix receipt

--- ConfirmationEmailContent.js
+++ ConfirmationEmailContent.js
@@ -1,9 +1,5 @@
-let confirmationMailConetnt = `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <style>
+let confirmationMailConetnt = `
+<style>
     body {
       font-family: 'Arial', sans-serif;
       background-color: #f0f4f8;
@@ -43,8 +39,6 @@ let confirmationMailConetnt = `<!DOCTYPE html>
     <p>Museum Name: {{museumName}}</p>
     <p>Tickets: {{noOfTickets}}</p>
     <p>Time: {{time}}</p>
-  </div>
-</body>
-</html>`
+  </div>`
 
 module.exports = confirmationMailConetnt
